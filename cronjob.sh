e7bd1c91d298f24c2a971a3766450a1317cba7ed
palani <palani@convertcart.com>
2018-12-11 11:04:16 +0530

    minor improvements

--- cronjob.sh
+++ cronjob.sh
@@ -1,3 +1,4 @@
+set -e
 # required ENV variables
 # SERVICE_NAME - name of container which generates log
 # TARGET_BUCKET - bucket to which file should be copied
@@ -25,8 +26,8 @@ echo sending USR1 signal to container $CONTAINER_ID
 docker kill --signal=USR1 $CONTAINER_ID
 
 # copy file to gs
-GS_DIR=gs://$TARGET_BUCKET/$BK_YEAR/$BK_MONTH/$BK_DAY/
-gcloud -q auth activate-service-account --key-file=/gcloud-auth.json
+GS_DIR=gs://$TARGET_BUCKET/$BK_YEAR/$BK_MONTH/$BK_DAY
+gcloud --no-user-output-enabled  auth activate-service-account --key-file=/gcloud-auth.json
 echo copying $GS_DIR/$OUT_FILE
 gsutil -q cp -Z $OUT_FILE $GS_DIR/$OUT_FILE
 rm $OUT_FILE
